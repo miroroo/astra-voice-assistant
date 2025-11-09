21911c74b4d3b3bbd91846e473a59a61db37ee7b
miroroo <miroror173.173@gmail.com>
2025-11-09 10:50:01 +0300

    docs: обновлён launch.bat под виртуальное окружение + добавлены зависимости vosk и sounddevice

--- launch.bat
+++ launch.bat
@@ -1,7 +1,13 @@
 @echo off
 echo === launch Astra ===
-cd /d "C:\Users\dzhir\Desktop\prog\astra-voice-assistant"
+cd /d "ВАШ ПУТЬ ДО astra-voice-assistant"
 echo Folder: %CD%
 echo.
+
+:: Активируем виртуальное окружение
+call venv\Scripts\activate.bat
+
+echo.
+echo Starting application...
 python -m src.main
 pause
