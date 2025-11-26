5e495d798647990f4353693a9903ad5a4795d309
len <dariatrent975@gmail.com>
2025-11-26 22:55:18 +0300

    удалила большую модель из коммита

--- src/voice_engine/listener.py
+++ src/voice_engine/listener.py
@@ -30,7 +30,7 @@ class VoiceModule:
         # Путь к модели
         if model_path is None:
             base_dir = os.path.dirname(os.path.abspath(__file__))
-            model_path = os.path.join(base_dir, "models", "vosk-model-small-ru-0.22")
+            model_path = os.path.join(base_dir, "models", "vosk-model-small-ru-0.42")
 
         if not os.path.exists(model_path):
             self.logger.critical(f"Модель не найдена по пути: {model_path}")
